db3db3cb6f88515429aaca8809dcdf795209177a
Nikita Liashenko <117355559+playnik94@users.noreply.github.com>
2023-01-20 13:51:47 +0200

    Update dz_2.py

--- dz_2.py
+++ dz_2.py
@@ -1,11 +1,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
+new = []
 with open('cities.csv') as f:
-    lines = f.readlines()
-    for line in lines:
-        print(str(line).replace("\n", " ").split(";"))
+    lines = f.readline()
+    for line in f:
+        new.append(line.replace("\n"," ").split(';'))
+print(new)
+
 
 Cities = [['Hadiach', 'Zinkiv', 45],['Zinkiv', 'Hadiach', 45],['Hadiach', 'Lebedyn', 60],
           ['Lebedyn', 'Hadiach', 60],['Hadiach', 'Myrhorod', 61],['Myrhorod', 'Hadiach', 61]]
